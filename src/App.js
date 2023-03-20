9595c9ee15a0c3f5896f9d0484d7e389c4727618
Rajiv-Kumar-Singh <rv488448@gmail.com>
2023-03-20 19:17:36 +0530

    add news categories

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 export default class App extends Component {
   render() {
     return (
       <>
-        <Navbar />
-        <News pageSize={5} />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News key="general" pageSize={6} country="in" category="general" headline="Top Headlines" />} />
+
+            <Route exact path="/business" element={<News key="business" pageSize={6} country="in" category="business" headline="Bunsiness" />} />
+
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={6} country="in" category="entertainment" headline="Entertainment" />} />
+
+            <Route exact path="/health" element={<News key="health" pageSize={6} country="in" category="health" headline="Health" />} />
+
+            <Route exact path="/science" element={<News key="science" pageSize={6} country="in" category="science" headline="Science" />} />
+
+            <Route exact path="/sports" element={<News key="sports" pageSize={6} country="in" category="sports" headline="Sports" />} />
+
+            <Route exact path="/technology" element={<News key="technology" pageSize={6} country="in" category="technology" headline="Technology" />} />
+          </Routes>
+        </Router>
       </>
     )
   }
--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
+import PropTypes from 'prop-types'
 
 class News extends Component {
+    static defaultProps = {
+        country: 'in',
+        pageSize: 8,
+        category: 'general'
+    }
+
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string
+    }
+
     constructor(props) {
         super(props)
 
@@ -14,7 +27,7 @@ class News extends Component {
     }
 
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6b19f7a8d6a140a899c9cf8afeb69c13&page=1&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6b19f7a8d6a140a899c9cf8afeb69c13&page=1&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json()
@@ -22,7 +35,7 @@ class News extends Component {
     }
 
     handlePrevClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6b19f7a8d6a140a899c9cf8afeb69c13&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6b19f7a8d6a140a899c9cf8afeb69c13&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json()
@@ -40,7 +53,7 @@ class News extends Component {
 
     handleNextClick = async () => {
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6b19f7a8d6a140a899c9cf8afeb69c13&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6b19f7a8d6a140a899c9cf8afeb69c13&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             this.setState({ loading: true })
             let data = await fetch(url);
             let parsedData = await data.json()
@@ -61,7 +74,7 @@ class News extends Component {
     render() {
         return (
             <div className='container my-3'>
-                <h2 className='text-center'>Top Headlines</h2>
+                <h2 className='text-center fw-bold my-3'>{this.props.headline}</h2>
                 {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
